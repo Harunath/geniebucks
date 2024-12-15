b6001714d98b8c90c24a61073b4e72ac28d47882
harunath <harunath04@gmail.com>
2024-12-15 13:08:32 +0530

    aurth and font bug

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import useStore from "@/store/state";
+import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
 const Providers = ({ children }: { children: ReactNode }) => {
 	const dark = useStore((state) => state.dark);
-	return <div className={`${dark ? "dark" : ""}`}>{children}</div>;
+	return (
+		<SessionProvider>
+			<div className={`${dark ? "dark" : ""}`}>{children}</div>;
+		</SessionProvider>
+	);
 };
 
 export default Providers;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import { DarkMode } from "./home/DarkMode";
+import Auth from "./Auth";
 
 export default function Navbar() {
 	return (
 		<nav className="bg-old_lace dark:bg-[#4B5945] dark:text-neutral-200 transition duration-500 py-4">
 			<div className="container mx-auto flex justify-between items-center px-4">
-				<h1 className="text-lg font-bold">GenieBucks</h1>
+				<Link
+					href="/"
+					className="text-2xl font-bold text-neutral-900 dark:text-neutral">
+					<h1 className="text-lg font-bold">GenieBucks</h1>
+				</Link>
 				<ul className="flex space-x-4">
 					<li>
 						<Link href="/" className="hover:underline">
@@ -24,6 +29,9 @@ export default function Navbar() {
 					</li>
 					<li>
 						<DarkMode />
+					</li>
+					<li>
+						<Auth />
 					</li>
 				</ul>
 			</div>
