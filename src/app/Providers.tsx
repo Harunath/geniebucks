9f8b6dbc98b28a6fea7991c2301dbbb3bd1caa81
harunath <harunath04@gmail.com>
2025-05-01 16:05:46 +0530

    tailwindcss upgrade

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,16 +1,20 @@
 "use client";
 
-import useStore from "@/store/state";
+// import useStore from "@/store/state";
 import { SessionProvider } from "next-auth/react";
 import { ToastContainer } from "react-toastify";
 import { ReactNode } from "react";
 
 const Providers = ({ children }: { children: ReactNode }) => {
-	const dark = useStore((state) => state.dark);
+	// const dark = useStore((state) => state.dark);
 	return (
 		<SessionProvider>
 			<ToastContainer />
-			<div className={`${dark ? "dark" : ""}`}>{children}</div>;
+			{/* <div className={`${dark ? "dark" : ""}`}> */}
+
+			<div>{children}</div>
+
+			{/* </div>; */}
 		</SessionProvider>
 	);
 };
